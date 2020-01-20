000d67d8bcfa49928d97333b9d89c56f260146f4
marios <marios.katsakioris@gmail.com>
2020-01-20 17:09:08 +0000

    output dir

--- run/vqa_finetune.bash
+++ run/vqa_finetune.bash
@@ -2,7 +2,7 @@
 name=$2
 
 # Save logs and models under snap/vqa; make backup.
-output=snap/vqa/$name
+output=/scratch/mmk11/snap/vqa/$name
 mkdir -p $output/src
 cp -r src/* $output/src/
 cp $0 $output/run.bash
@@ -12,6 +12,6 @@ CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/vqa.py \
     --train train,nominival --valid minival  \
     --llayers 9 --xlayers 5 --rlayers 5 \
-    --loadLXMERTQA snap/pretrained/model \
+    --loadLXMERTQA /scratch/mmk11/snap/pretrained/model \
     --batchSize 32 --optim bert --lr 5e-5 --epochs 4 \
     --tqdm --output $output ${@:3}

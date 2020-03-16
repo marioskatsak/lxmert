d1fc4a681f3eeb6d48817814b287a1af3ee9edcb
marios <marios.katsakioris@gmail.com>
2020-03-16 13:05:05 +0000

    import

--- src/tasks/vqa_data.py
+++ src/tasks/vqa_data.py
@@ -10,7 +10,7 @@ import torch
 from torch.utils.data import Dataset
 
 from param import args
-from utils import load_obj_tsv
+from utils import load_obj_tsv, load_det_obj_tsv
 
 # Load part of the dataset for fast checking.
 # Notice that here is the number of images instead of the number of data,

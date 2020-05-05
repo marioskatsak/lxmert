022486a4c8389c150f4c15cfd2c3a16b24b2af53
marios <marios.katsakioris@gmail.com>
2020-05-05 18:27:21 +0100

    adadd

--- src/lxrt/modeling.py
+++ src/lxrt/modeling.py
@@ -794,8 +794,8 @@ class VisualFeatEncoder(nn.Module):
             # output = x*y*z
             # output = (x + y + z) / 3
         else:
-            # output = (x + y) / 2
-            output = y
+            output = (x + y) / 2
+            # output = y
         # input(output.shape)
         output = self.dropout(output)
         return output

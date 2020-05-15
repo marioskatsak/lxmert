943225d63d0dfaf6c00d64935194b38ff28876c6
marios <marios.katsakioris@gmail.com>
2020-05-15 19:32:06 +0100

    dad

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -15,5 +15,5 @@ CUDA_VISIBLE_DEVICES=2 PYTHONPATH=$PYTHONPATH:./src \
     --train train --valid valid \
     --llayers 1 --xlayers 1 --rlayers 1 \
     --dataPath /scratch/mmk11/data/rosmi/ \
-    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 250 --abla t_Names_Boxes \
+    --batchSize 20 --optim bert --lr 1e-3 --epochs 250 --abla t_Boxes \
     --tqdm --output $output ${@:3}

fe6fdd339782ed244b59b57d00addea9a3831a9a
marios <marios.katsakioris@gmail.com>
2020-05-15 19:34:21 +0100

    adasd

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -10,10 +10,11 @@ cp $0 $output/run.bash
 # See Readme.md for option details.
 #--loadLXMERT snap/pretrained/model \
 #--fromScratch \
-CUDA_VISIBLE_DEVICES=3 PYTHONPATH=$PYTHONPATH:./src \
+CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
     --train train --valid valid \
     --llayers 1 --xlayers 1 --rlayers 1 \
+    --fromScratch \
     --dataPath /scratch/mmk11/data/rosmi/ \
-    --batchSize 20 --optim bert --lr 1e-3 --epochs 250 --abla t_Boxes \
+    --batchSize 20 --optim bert --lr 1e-3 --epochs 250 --abla t_scr_Names_Boxes \
     --tqdm --output $output ${@:3}

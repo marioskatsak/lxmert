16ef9e348356695ee1f2ec9f351283ee3be12389
marios <marios.katsakioris@gmail.com>
2020-05-16 16:57:09 +0100

    as a a

--- src/lxrt/modeling.py
+++ src/lxrt/modeling.py
@@ -790,8 +790,8 @@ class VisualFeatEncoder(nn.Module):
             # print(y.shape)
             # print(y.shape)
             # input(z.shape)
-            # output = (x+y)*z
-            output = y*z
+            output = (x+y)*z
+            # output = y*z
             # output = z
             # output = (x + y + z) / 3
         else:

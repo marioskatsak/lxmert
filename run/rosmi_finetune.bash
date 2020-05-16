19a46413516f083d9b2bc6cc50d23ca977394455
marios <marios.katsakioris@gmail.com>
2020-05-16 16:14:04 +0100

    transformer

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -10,10 +10,10 @@ cp $0 $output/run.bash
 # See Readme.md for option details.
 #--loadLXMERT snap/pretrained/model \
 #--fromScratch \
-CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PYTHONPATH:./src \
+CUDA_VISIBLE_DEVICES=2 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
     --train train --valid valid \
-    --llayers 1 --xlayers 1 --rlayers 1 \
+    --llayers 0 --xlayers 0 --rlayers 0 \
     --dataPath /scratch/mmk11/data/rosmi/ \
-    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 250 --abla T_Names \
+    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 250 --abla t_trans_Names \
     --tqdm --output $output ${@:3}

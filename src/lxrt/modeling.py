f4f49dd3e6ffb017cc6b0f9623201f659735a2fe
marios <marios.katsakioris@gmail.com>
2020-05-17 15:55:28 +0100

    names boxes

--- src/lxrt/modeling.py
+++ src/lxrt/modeling.py
@@ -791,13 +791,13 @@ class VisualFeatEncoder(nn.Module):
             # print(y.shape)
             # input(z.shape)
             # output = (x+y)*z
-            # output = y*z
-            output = x*z
+            output = y*z
+            # output = x*z
             # output = z
             # output = (x + y + z) / 3
         else:
-            # output = (x + y) / 2
-            output = y
+            output = (x + y) / 2
+            # output = y
         # input(output.shape)
         output = self.dropout(output)
         return output

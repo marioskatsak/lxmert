9a9781fd20027b4ab01d5159ef8982b021e3dc89
marios <marios.katsakioris@gmail.com>
2020-05-20 22:47:38 +0100

    loolf f

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -12,7 +12,7 @@ cp $0 $output/run.bash
 CUDA_VISIBLE_DEVICES=2 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
     --train train --valid valid \
-    --llayers 1 --xlayers 1 --rlayers 1 \
+    --llayers 0 --xlayers 0 --rlayers 0 \
     --dataPath /scratch/mmk11/data/rosmi/ \
-    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 200 --abla t_Names \
+    --batchSize 20 --optim bert --lr 1e-3 --epochs 200 --abla t_trans_Boxes \
     --tqdm --output $output ${@:3}

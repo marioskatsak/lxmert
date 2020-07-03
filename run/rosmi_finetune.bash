17cf24297a9c30526a8d82b655e6924c77d0a58f
marios <marios.katsakioris@gmail.com>
2020-07-03 22:31:00 +0100

    lloading and train

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -12,7 +12,7 @@ cp $0 $output/run.bash
 CUDA_VISIBLE_DEVICES=2 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
     --train train --valid valid --load /scratch/mmk11/snap/rosmi/BEST_1_t_NAME \
-    --llayers 1 --xlayers 1 --rlayers 1 --single \
+    --llayers 1 --xlayers 1 --rlayers 1 \
     --dataPath /scratch/mmk11/data/rosmi/ \
     --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 100 --abla 1_t_NAME \
     --tqdm --output $output ${@:3}

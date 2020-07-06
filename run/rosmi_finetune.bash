adc0e310c45395e9812f389fe9c1d2a756196cad
marios <marios.katsakioris@gmail.com>
2020-07-06 23:29:39 +0100

    pleaseee

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -8,11 +8,11 @@ cp -r src/* $output/src/
 cp $0 $output/run.bash
 
 # See Readme.md for option details.
-#--loadLXMERT snap/pretrained/model \--load /scratch/mmk11/snap/rosmi/BEST_1_t_NAME
+#--loadLXMERT snap/pretrained/model \
 CUDA_VISIBLE_DEVICES=2 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
-    --train train --valid valid  \
-    --llayers 1 --xlayers 1 --rlayers 1 \
+    --train train --valid valid --load /scratch/mmk11/snap/rosmi/BEST_2_t_NAME \
+    --llayers 1 --xlayers 1 --rlayers 1 --single \
     --dataPath /scratch/mmk11/data/rosmi/ \
-    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 100 --abla 2_t_NAME \
+    --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 100 --abla 2_t_NAME \
     --tqdm --output $output ${@:3}

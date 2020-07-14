d9e4d6ba3a7cc7d2a65b5beb578488f98f5123bc
marios <marios.katsakioris@gmail.com>
2020-07-14 23:14:13 +0100

    asd asd sa MM

--- src/tasks/rosmi_data.py
+++ src/tasks/rosmi_data.py
@@ -1239,9 +1239,9 @@ class RENCIEvaluator:
             else:
                 counterDist +=1
 
-            if siou2 > thres or siou > thres or siou3 > thres:
-
-                tScore += 1
+            # if siou2 > thres or siou > thres or siou3 > thres:
+            #
+            #     tScore += 1
 
             if pred_cland_coords:
                 save_land = str(names[ln_])

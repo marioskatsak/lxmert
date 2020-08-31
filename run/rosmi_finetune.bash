cfa9ba26da19a9030cd76d4dfa4a63c6a6f09158
marios <marios.katsakioris@gmail.com>
2020-09-01 00:48:21 +0100

    a sda sasss

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -11,8 +11,15 @@ cp $0 $output/run.bash
 #--loadLXMERT snap/pretrained/model \
 CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
-    --train train --valid valid --load BEST_load_k_RENCI_NAME \
-    --llayers 1 --xlayers 1 --rlayers 1 --single \
+    --train train --valid valid \
+    --llayers 1 --xlayers 1 --rlayers 1 \
     --dataPath /scratch/mmk11/data/renci/k \
-    --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla load_k_RENCI_NAME \
+    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla load_k_RENCI_NAME \
     --tqdm --output $output ${@:3}
+# CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
+#     python src/tasks/rosmi.py \
+#     --train train --valid valid --load BEST_load_k_RENCI_NAME \
+#     --llayers 1 --xlayers 1 --rlayers 1 --single \
+#     --dataPath /scratch/mmk11/data/renci/k \
+#     --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla load_k_RENCI_NAME \
+#     --tqdm --output $output ${@:3}

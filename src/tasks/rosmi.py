4ad375df045eee407aa65ef19a4d7c9582639322
marios <marios.katsakioris@gmail.com>
2020-09-02 19:02:55 +0100

    a s aaaa

--- src/tasks/rosmi.py
+++ src/tasks/rosmi.py
@@ -592,8 +592,8 @@ if __name__ == "__main__":
                 index_l = clnd[0]
                 land_tokens = nlp(land_name)
                 for lan_in,tn in enumerate(names):
-                    print(tn)
-                    tmp_n = nlp(tn)
+                    print(tn[0])
+                    tmp_n = nlp(tn[0])
                     if tmp_n.similarity(land_tokens) > 0.98:
                         index_l = lan_in
 

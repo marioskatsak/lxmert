c39a4a71161d20a0f06a4ad0e1a6e0960157c147
marioskatsak <marios.katsakioris@gmail.com>
2021-03-20 20:11:36 +0200

    testing rosmi

--- run/rosmi_finetune_testing.bash
+++ run/rosmi_finetune_testing.bash
@@ -10,10 +10,10 @@ cp $0 $output/run.bash
 # See Readme.md for option details.
 CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
-    --train 4_train_enc --valid 4_val_enc --load snap/rosmi/BEST_load_k_RENCI_NAME \
+    --train 4_train_enc --valid 4_val_enc --load snap/rosmi/BEST_k_RENCI_NAME \
     --llayers 1 --xlayers 1 --rlayers 1 \
-    --dataPath data/renci/k --single \
-    --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla load_k_RENCI_NAME \
+    --dataPath data/renci/k \
+    --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla load_k_RENCI_NAME \
     --tqdm --output $output ${@:3}
 
   # CUDA_VISIBLE_DEVICES=3 PYTHONPATH=$PYTHONPATH:./src \

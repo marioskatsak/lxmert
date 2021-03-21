bfe2f03c4681ecfdbfe2c9c7654dea424fcf7a6a
marioskatsak <marios.katsakioris@gmail.com>
2021-03-21 02:05:45 +0200

    testing rosmi

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -9,10 +9,10 @@ cp $0 $output/run.bash
 
 # See Readme.md for option details.
 #--loadLXMERT snap/pretrained/model \
-CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
+CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/rosmi.py \
     --train 0_train --valid 0_val \
     --llayers 2 --xlayers 1 --rlayers 2 \
     --dataPath /scratch/mmk11/data/rosmi/7_easy_train \
-    --batchSize 32 --optim bert --lr 1e-4 --n_ent --epochs 100 --abla random_t_NAME \
+    --batchSize 32 --optim bert --lr 1e-5 --n_ent --epochs 100 --abla random_t_NAME \
     --tqdm --output $output ${@:3}

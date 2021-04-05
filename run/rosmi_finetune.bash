31ce5b8850bb98e83c3b0b80ad2a8b812eadda5f
marioskatsak <marios.katsakioris@gmail.com>
2021-04-05 20:53:20 +0300

    trying val rosmi

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -22,5 +22,5 @@ cp $0 $output/run.bash
         --valid 0_val --load /scratch/mmk11/snap/rosmi/BEST_k_RENCI_NAME \
         --llayers 1 --xlayers 1 --rlayers 1\
         --dataPath /scratch/mmk11/data/rosmi/7_easy_train \
-        --batchSize 20 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla BEST_2_t_NAME \
+        --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla BEST_2_t_NAME \
         --tqdm --output $output ${@:3}

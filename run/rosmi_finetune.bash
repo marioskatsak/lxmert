d4e1886b645ddb127548d67f16448b1458b6a5c3
marioskatsak <marios.katsakioris@gmail.com>
2021-04-07 18:53:52 +0300

    trying val rosmi

--- run/rosmi_finetune.bash
+++ run/rosmi_finetune.bash
@@ -20,7 +20,7 @@ cp $0 $output/run.bash
     CUDA_VISIBLE_DEVICES=0 PYTHONPATH=$PYTHONPATH:./src \
         python src/tasks/rosmi.py \
         --valid 0_val --load /scratch/mmk11/snap/rosmi/BEST_k_RENCI_NAME \
-        --llayers 1 --xlayers 1 --rlayers 1\
+        --llayers 1 --xlayers 1 --rlayers 1 --cross \
         --dataPath /scratch/mmk11/data/rosmi/7_easy_train \
-        --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla BEST_2_t_NAME \
+        --batchSize 1 --optim bert --lr 1e-3 --n_ent --epochs 80 --abla hybrid_rule_based \
         --tqdm --output $output ${@:3}
